28fc8bd91861272cd2abaeff055a807fdc3498f0
Mamankmarcel <57529908+Mamankmarcel@users.noreply.github.com>
2019-11-09 14:54:39 +0700

    Add files via upload

---
+++ SendVirus.sh
@@ -0,0 +1,40 @@
+
+#!/bin/sh
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+bln="\033[33m"
+bc="\033[0m"
+
+echo "$grey ========="
+echo "$red SendVirus"
+echo "$cafe ========="
+sleep 1
+echo "apakah kamu ingin mengerjai teman?? "
+sleep 2
+echo "selamat datang di, $green Send Virus"
+sleep 2
+echo "disini virusnya tidak main main! "
+sleep 1
+echo "mohon untuk tidak mencoba di nomor anda!! "
+sleep 2
+echo "menuju pengisian virus.. "
+sleep 4
+read -p "nomor: " nomor
+echo "mencari $nomor "
+sleep 3
+echo " mendapatkan nomor $nomor "
+sleep 3
+echo " $nomor sudah diisi virus, jgn beritau ke siapapun"
+echo "agar iamu tidak dibenci"
+sleep 10
+exit
